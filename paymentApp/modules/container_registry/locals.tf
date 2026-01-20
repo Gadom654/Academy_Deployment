c575b75af5ec20c1c5af29c2d195635666d3df6b
gadom654 <dominikgadomski654@gmail.com>
2026-01-20 12:37:45 +0100

    formating terraform files

--- paymentApp/modules/container_registry/locals.tf
+++ paymentApp/modules/container_registry/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  container_registry_name           = "${var.prefix}acr"
-  container_registry_sku_standard   = "Basic"
-  container_registry_admin_enabled  = false
-  retention_policy_in_days = 7
-  isTrustPolicyEnabled = true
+  container_registry_name          = "${var.prefix}acr"
+  container_registry_sku_standard  = "Basic"
+  container_registry_admin_enabled = false
+  retention_policy_in_days         = 7
+  isTrustPolicyEnabled             = true
 }
