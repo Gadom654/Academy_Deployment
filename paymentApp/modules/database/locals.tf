c3e666078955969ddfc8c8dcd97e006d5b8bf101
gadom654 <dominikgadomski654@gmail.com>
2026-01-22 15:06:39 +0100

    fixing db values

--- paymentApp/modules/database/locals.tf
+++ paymentApp/modules/database/locals.tf
@@ -6,9 +6,9 @@ locals {
   db_pass_override_special    = "!#$%&*()-_=+[]{}<>:?"
   db_password_name            = "${var.prefix}-db-password"
   payment_db_server_name      = "${var.prefix}-payment-db"
-  payment_db_version          = "13.0"
+  payment_db_version          = "18"
   payment_db_storage_mb       = 32768
-  payment_db_sku_name         = "Standard_B1ms"
+  payment_db_sku_name         = "B_Standard_B1ms"
   payment_db_zone             = "1"
   payment_db_ha_mode          = "ZoneRedundant"
   payment_db_ha_standby_az    = "2"
