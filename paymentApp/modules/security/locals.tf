65b13bea61700c71fbfbb185bcdede4bcc68ae31
gadom654 <dominikgadomski654@gmail.com>
2026-01-22 13:33:23 +0100

    adding Azure Policy

--- paymentApp/modules/security/locals.tf
+++ paymentApp/modules/security/locals.tf
@@ -9,4 +9,20 @@ locals {
       "Purge",
       "Recover"
     ]
+  allow-eu_only_name = "${var.prefix}-allow-eu-only"
+  allow-eu_only_policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/e56962a6-4747-49cd-b67b-bf8b01975c4c"
+  allow-eu_only_display_name = "Enforce EU Regions Only"
+  allow-eu_only_description = "Restricts resource creation in this Resource Group to European regions only."
+  allow-eu_only_parameters = <<PARAMETERS
+  {
+  "listOfAllowedLocations": {
+    "value": [
+      "westeurope",
+      "northeurope",
+      "francecentral",
+      "germanywestcentral"
+    ]
+  }
+}
+PARAMETERS
 }
--- paymentApp/modules/security/main.tf
+++ paymentApp/modules/security/main.tf
@@ -22,4 +22,16 @@ resource "azurerm_key_vault" "kv" {
     secret_permissions = local.kv_secret_permissions
   }
 }
-
+################
+# Azure Policy #
+################
+resource "azurerm_resource_group_policy_assignment" "allow-eu_only" {
+  name                 = local.allow-eu_only_name
+  location             = var.location
+  resource_group_id    = var.resource_group_name
+  policy_definition_id = local.allow-eu_only_policy_definition_id
+  
+  display_name = local.allow-eu_only_display_name
+  description  = local.allow-eu_only_description
+  parameters   = local.allow-eu_only_parameters
+}
