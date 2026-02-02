9831d40e15faa90e229e28ab980f85b3fa64b940
gadom654 <dominikgadomski654@gmail.com>
2026-02-02 12:34:13 +0100

    added backend possibility

---
+++ AWS/ECSapp/terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-states-gadom"
+    key            = "ecs/terraform.tfstate"
+    region         = "eu-north-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
