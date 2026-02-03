25d461bf350d79d11588d2260dabd17499c24075
gadom654 <dominikgadomski654@gmail.com>
2026-02-03 11:40:50 +0100

    adding autoscaling policy

--- AWS/ECStroubleshoot/13_autoscaling.tf
+++ AWS/ECStroubleshoot/13_autoscaling.tf
@@ -9,3 +9,20 @@ resource "aws_appautoscaling_target" "web" {
   max_capacity       = var.web_max_capacity
 }
 
+resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
+  name               = "cpu-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.web.resource_id
+  scalable_dimension = aws_appautoscaling_target.web.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.web.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = var.cpu_target_percent
+    scale_in_cooldown  = 60   
+    scale_out_cooldown = 20    
+  }
+}
